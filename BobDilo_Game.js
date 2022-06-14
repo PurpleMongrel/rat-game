f92d8bce7230976d7a0d35698edca8de56a6ff81
PurpleMongrel <thaispdf@gmail.com>
2022-06-13 22:31:38 -0400

    Revert "renamed startScreenCounter -> levelIntroDuration. Some minor debugging logging/refactoring"
    This reverts commit 86bbf1df50d512af4d76ac0555fb4eadaca08dea.
    
    something went wrong. reverting

--- BobDilo_Game.js
+++ BobDilo_Game.js
@@ -593,22 +593,10 @@ class Bullet {
   update(timeElapsed, state, index) {
     this.position.y -= timeElapsed * 0.5;
     this.counter += timeElapsed;
-    console.log(this.counter)
-    console.log(this.position)
     if (this.counter > 400) {
       this.remove = true;
     }
-    return new Bullet(this.position, this.target, this.counter, this.duration, this.remove)
-
-
-    /* this.position.y -= timeElapsed * 0.5;
-    this.counter += timeElapsed;
-    let newPos = {"x": this.position.x, "y": this.position.y - timeElapsed * 0.5};
-    let newCounter = this.counter += timeElapsed;
-    if (this.counter > 400) {
-      this.remove = true;
-    }
-    return Bullet.create(newPos, this.target, newCounter, this.duration, this.remove) */
+    return Bullet.create(this.position, this.target, this.counter, this.duration, this.remove)
 
   };
 
@@ -804,7 +792,7 @@ class State {
     for (let i = 0; i < this.characters.length; i++) {
 
       let newChar = this.characters[i].update(timeElapsed, state, i);
-if (newChar.type == "bullet") console.log(newChar.counter)
+
       //Make sure bullets who have expired do not get passed into updated state.characters
       if (
         newChar.type != "bullet" ||
@@ -936,18 +924,19 @@ function runLevel(levelsArray, levelIndex) {
       levelIntroTimer += timeElapsed;
 
       if (state.gameData.levelIntroDone == true) {
-        state = state.update(timeElapsed, state)
+        //state = state.update(timeElapsed, state)
+
       }
-      let levelIntroDuration;
+      let startScreenCounter;
 
       //levelIntroTimer situation seems unecessarily messy. Needs refactoring
       if (
         state.gameData.levelIntroDone == false &&
         state.gameData.level == 0
       ) {
-        levelIntroDuration = 500;
+        startScreenCounter = 500;
       }
-      if (levelIntroTimer > levelIntroDuration) {
+      if (levelIntroTimer > startScreenCounter) {
         state.gameData.levelIntroDone = true;
       }
       gameCanvas.syncCanvasToState(state);
